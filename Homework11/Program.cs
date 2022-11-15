25fa8415bee6dfb26dd1bac4b284ff71851b4a57
Dmitriy Baranov <bdvbuild@gmail.com>
2022-11-15 18:35:08 +0400

    Добавьте файлы проекта.

---
+++ Homework11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Уравнение вида kx+b=0");
+            bool endApp;
+
+            do
+            {
+                try
+                {
+                    Console.Write("\nВведите k: ");
+                    double k = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите b: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+
+                    Linear linear = new Linear(k, b);
+                    Console.WriteLine(linear.Root());
+                    endApp = true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    endApp = false;
+                }
+            } while (!endApp);
+            Console.ReadKey();
+        }
+    }
+}
